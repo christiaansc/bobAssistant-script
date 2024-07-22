ed2ee9723c083ffc824e9f62bd1e034b2ad9f4df
Christian <cristiaan.sc@gmail.com>
2024-07-22 13:24:32 -0400

    Add while in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from mqtt_client import MQTTClient
+import time
+
 
 
 if __name__ == "__main__":
     mqtt_client = MQTTClient()
-    mqtt_client.connect()
+    
+    while True:
+        try:
+            mqtt_client.connect()
+        except Exception as e:
+            print(f"Error: {e}")
+            time.sleep(10)  # Espera 10 segundos antes de intentar reconectar
 
 
